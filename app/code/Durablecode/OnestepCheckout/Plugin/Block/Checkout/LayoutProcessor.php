08e10fdb2f96290a2822283ffc205aed518bc94d
Wojciech Nowak <kontakt@wnowak.pl>
2020-11-17 15:11:02 +0100

    Implement form to billing address

---
+++ app/code/Durablecode/OnestepCheckout/Plugin/Block/Checkout/LayoutProcessor.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace Durablecode\OnestepCheckout\Plugin\Block\Checkout;
+
+use Magento\Checkout\Block\Checkout\LayoutProcessor as CheckoutLayoutProcessor;
+
+/**
+ * Class LayoutProcessor
+ * @package Durablecode\OnestepCheckout\Plugin
+ */
+class LayoutProcessor
+{
+    /**
+     * Solved problem with lack data for dc-billing-address components
+     *
+     * @param CheckoutLayoutProcessor $instance
+     * @param array $result
+     * @return array
+     */
+    public function afterProcess(CheckoutLayoutProcessor $instance, array $result): array
+    {
+        /**
+         * Temporary solution
+         * @todo solved problem with billing address form for different shipping methods
+         */
+        $billingFields = $result['components']['checkout']['children']['steps']['children']['billing-step']['children']['payment']['children']['payments-list']['children']['paypal_billing_agreement-form']['children']['form-fields']['children'];
+
+        $result['components']['checkout']['children']['steps']['children']['dc-billing-address']['children']['additional-fieldsets']['children'] = $billingFields;
+
+        return $result;
+    }
+}
